d15c72d4b863ba56d2726490a511ab9dbd22269f
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-10 16:32:38 +0900

    more fixing of watches

--- docker/src/App.vue
+++ docker/src/App.vue
@@ -7,7 +7,7 @@
           title="Home"
           :to="{ name: 'tables' }"
         />
-        <v-divider></v-divider>
+        <v-divider />
         <v-list-item
           v-for="(table, index) in tables"
           :key="`table_${index}`"
@@ -21,22 +21,16 @@
 </template>
 
 <script setup>
-// TODO: add vuetify3 application template
-
 import AppTemplate from "@moreillon/vuetify3-application-template";
 import "@moreillon/vuetify3-application-template/dist/style.css";
 import { ref, onMounted } from "vue";
 import axios from "axios";
 
-const {
-  VITE_TITLE = "Auto CRUD",
-  VITE_API_URL,
-  VITE_LOGIN_URL,
-  VITE_IDENTIFICATION_URL,
-} = import.meta.env;
+const { VITE_TITLE, VITE_API_URL, VITE_LOGIN_URL, VITE_IDENTIFICATION_URL } =
+  import.meta.env;
 
 const options = ref({
-  title: VITE_TITLE,
+  title: "Auto CRUD",
   author: "Maxime Moreillon",
   login_url: VITE_LOGIN_URL,
   identification_url: VITE_IDENTIFICATION_URL,
