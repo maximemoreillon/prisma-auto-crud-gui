fff3b07949f373b477bf363c648f664378afe257
Maxime Moreillon <m.moreillon@gmail.com>
2024-02-26 08:35:26 +0900

    missing dep

--- docker-test.sh
+++ docker-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+
+
+docker build -t auto-crud-gui . && \
 docker run \
 -e VITE_API_URL=http://172.16.98.151:7070 \
 -p 8080:80 \
-moreillon/prisma-auto-crud-gui:latest
+auto-crud-gui
